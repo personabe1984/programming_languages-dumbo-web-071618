a0bbc71f5cb87f38ffca88c5fe76390b2ba4cd6d
Hanbi Hans Choi <personabe1984@gmail.com>
2018-07-11 22:01:23 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -8,7 +8,7 @@ def reformat_languages(languages)
       end
     else
       langs.each do |specific_lang, data|
-        if new_hash[specific_lang].has_key? :syle
+        if new_hash[specific_lang].has_key? :style
           puts new_hash[specific_lang][:syle]
         else
           new_hash[specific_lang] = data
